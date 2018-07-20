f9915320f786e56c4ec61e03d9ef54cbd5685675
Matthew Veres <mveres@gra-login3.graham.sharcnet>
2018-07-20 12:07:11 -0400

    adding reservation

--- submit_job.sh
+++ submit_job.sh
@@ -4,6 +4,7 @@
 #SBATCH --mem=4000M                   # memory (per node)
 #SBATCH --time=0-00:10                # time (DD-HH:MM)
 #SBATCH --output=slurm-%j.out         # output filename pattern; j == jobid
+#SBATCH --reservation=guelph_workshop
 module load miniconda3
 source activate pytorch4
 python main.py --no-progress
